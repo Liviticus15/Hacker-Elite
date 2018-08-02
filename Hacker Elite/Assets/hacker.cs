c97befc9a4d87b8e5461c1cdf53e7d0bd1335414
DESKTOP-7PN176G\Admin <leviticuse2000@gmail.com>
2018-08-02 17:59:21 -0500

    Moving project to cloud from raquel's computer

---
+++ Hacker Elite/Assets/hacker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start  () {
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu (){
+        Terminal.ClearScreen ();
+        var greeting = "Login Complete...";
+        Terminal.WriteLine(greeting);
+        Terminal.WriteLine("Welcome User");
+        Terminal.WriteLine("" +
+            "");
+        Terminal.WriteLine("Which System You Like To Hack Into?");
+        Terminal.WriteLine("" +
+            "");
+        Terminal.WriteLine("Press 1 for Town Hall");
+        Terminal.WriteLine("Press 2 for State Capitol");
+        Terminal.WriteLine("Press 3 for Pentagon");
+        Terminal.WriteLine("" +
+            "");
+        Terminal.WriteLine("Enter Selection: ");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
